d198cdd5fbdfe88945303d335e04c4677ef04c13
palmseung <palmseung@gmail.com>
2020-12-29 01:13:26 +0900

    feat: add server setting

--- src/main/java/com/example/demogrpcserver/DemoGrpcServerApplication.java
+++ src/main/java/com/example/demogrpcserver/DemoGrpcServerApplication.java
@@ -1,13 +1,25 @@
 package com.example.demogrpcserver;
 
+import com.example.demogrpcserver.service.SampleServiceImpl;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import java.io.IOException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoGrpcServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		SpringApplication.run(DemoGrpcServerApplication.class, args);
+
+		Server server = ServerBuilder.forPort(8080)
+				.addService(new SampleServiceImpl())
+				.build();
+
+		server.start();
+
+		server.awaitTermination();
 	}
 
 }
