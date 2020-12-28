9d13a085861877ab51497194abe4d3f3b3646c4e
palmseung <palmseung@gmail.com>
2020-12-29 01:10:42 +0900

    feat: implement service

--- src/main/java/com/example/demogrpcserver/service/SampleServiceImpl.java
+++ src/main/java/com/example/demogrpcserver/service/SampleServiceImpl.java
@@ -1,10 +1,22 @@
 package com.example.demogrpcserver.service;
 
+import com.example.demogrpcserver.grpc.SampleRequest;
+import com.example.demogrpcserver.grpc.SampleResponse;
+import com.example.demogrpcserver.grpc.SampleServiceGrpc.SampleServiceImplBase;
+import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-public class SampleServiceImpl {
+public class SampleServiceImpl extends SampleServiceImplBase {
 
+  @Override
+  public void sampleCall(SampleRequest request, StreamObserver<SampleResponse> responseObserver) {
+    responseObserver.onNext(SampleResponse.newBuilder()
+        .setMessage("success")
+        .build());
+
+    responseObserver.onCompleted();
+  }
 }
