beeffe2a8c592ab0eaa1f5abafd6971c25e36329
palmseung <palmseung@gmail.com>
2020-12-29 00:23:00 +0900

    chore: init commit for demo-grpc-server

---
+++ src/main/java/com/example/demogrpcserver/DemoGrpcServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogrpcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGrpcServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGrpcServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demogrpcserver/DemoGrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogrpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGrpcServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
